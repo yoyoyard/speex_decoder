0e7969b05dc8a87f12d6cdff6c8072209ef20178
Xiaohui Gan <286390860@qq.com>
2018-11-23 12:39:24 +0800

    use more meanful error message for the decode method

--- lib/speex_decoder.rb
+++ lib/speex_decoder.rb
@@ -2,7 +2,18 @@ require "speex_decoder/version"
 require "speex_decoder/speex_decoder"
 
 module SpeexDecoder
-  def self.test
-    SpeexDecoder.decode '/Users/xiaohui/Desktop/test.speex', '/Users/xiaohui/Desktop/test.wav'
+  class << self
+    alias_method :origin_decode, :decode
+    def decode(speex_path, wav_path)
+      result = origin_decode(speex_path, wav_path)
+
+      if result == 0
+        return :success
+      elsif result == 1
+        raise '.speex file could not found.'
+      else
+        raise 'save file error for the output wav file.'
+      end
+    end
   end
 end
